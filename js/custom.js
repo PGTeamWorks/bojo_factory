668e00bbb4f3b35f742ca770a81ba0a66d099a05
Guilherme Felipe da Silva Reis <guilherme.reis@fcamara.com.br>
2015-03-15 03:54:23 -0300

    funcao de subtracao e calculos totais * ainda não terminei

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     $('.numero').mask("99999999");
 
+    var totalBojo = 0;
+    var acumTcVermelho = $('#');
+    var acumTcBranco = $('#');
+    var acumTcPreto = $('#');
+
     var placa = 8;
 
     var inputNumPedido = $('#numPedido');
@@ -11,6 +16,8 @@ $(document).ready(function () {
     var btnGravar = $('#btn-gravar');
 
     var pedidoList = $('#pedidoList');
+    var materialGastoList = $('#materialGastoList');
+
 
     var qtdeTecidoVermelho = $('#qtde_tecido_vermelho');
     var qtdeTecidoBranco = $('#qtde_tecido_branco');
@@ -52,6 +59,7 @@ $(document).ready(function () {
     };
 
     btnGravar.click(function(e){
+        materialGasto.espuma = 0;
         e.preventDefault();
 
         pedido.numPedido = inputNumPedido.val();
@@ -61,7 +69,9 @@ $(document).ready(function () {
         pedido.bojoPreto = inputBojoPreto.val();
 
         calculaPedido(pedido);
-        calculaMaterial(pedido.bojoVermelho);
+
+        espumaUtilizada.html(totalBojo.toFixed(1));
+
         $('html, body').animate({scrollTop: 300}, 300);
     });
 
@@ -72,23 +82,43 @@ $(document).ready(function () {
      */
     function calculaPedido(pedido) {
 
-        tecidoVermelhoUtilizado.html(materialGasto.tecidoVermelho);
-        tecidoBrancoUtilizado.html(materialGasto.tecidoBranco);
-        tecidoPretoUtilizado.html(materialGasto.tecidoPreto);
-        espumaUtilizada.html(materialGasto.espuma);
+        qtdeTecidoVermelho.html((estoque.tecido_vermelho - calculaTecido(calculaMaterial(pedido.bojoVermelho))).toFixed(1));
+        qtdeTecidoBranco.html((estoque.tecido_branco  - calculaTecido(calculaMaterial(pedido.bojoBranco))).toFixed(1));
+        qtdeTecidoPreto.html((estoque.tecido_preto - calculaTecido(calculaMaterial(pedido.bojoPreto))).toFixed(1));
+        qtdeEspuma.html((estoque.espuma -= materialGasto.espuma).toFixed(1))
 
         var boxPedido = '<div class="list-group-item">'+
-                            '<h4 class="list-group-item-heading page-header">Pedido Nº '+pedido.numPedido+', '+pedido.nomeCliente+'</h4>'+
-                            '<dl class="dl-horizontal">'+
-                            '<dt>Bojos vermelhos</dt>'+
-                                '<dd>'+pedido.bojoVermelho+'</dd>'+
-                            '<dt>Bojos Brancos</dt>'+
-                                '<dd>'+pedido.bojoBranco+'</dd>'+
-                            '<dt>Bojos Pretos</dt>'+
-                                '<dd>'+pedido.bojoPreto+'</dd>'+
-                            '</dl>'+
-                        '</div>';
+            '<h4 class="list-group-item-heading page-header">Pedido Nº '+pedido.numPedido+', '+pedido.nomeCliente+'</h4>'+
+            '<dl class="dl-horizontal">'+
+                '<dt>Bojos vermelhos</dt>'+
+                    '<dd>'+pedido.bojoVermelho+'</dd>'+
+                '<dt>Bojos Brancos</dt>'+
+                    '<dd>'+pedido.bojoBranco+'</dd>'+
+                '<dt>Bojos Pretos</dt>'+
+                    '<dd>'+pedido.bojoPreto+'</dd>'+
+                '</dl>'+
+            '</div>';
+
         pedidoList.append(boxPedido).hide().fadeIn(1000);
+
+        var boxMaterialGasto = '<div class="list-group-item">'+
+            '<h4 class="list-group-item-heading page-header">Pedido Nº '+pedido.numPedido+', '+pedido.nomeCliente+'</h4>'+
+            '<dl class="dl-horizontal mg-zero">'+
+            '<dt>Tecido vermelhos</dt>'+
+            '<dd>'+calculaTecido(calculaMaterial(pedido.bojoVermelho))+'</dd>'+
+            '<dt>Tecido Brancos</dt>'+
+            '<dd>'+calculaTecido(calculaMaterial(pedido.bojoBranco))+'</dd>'+
+            '<dt>Tecido Pretos</dt>'+
+            '<dd>'+calculaTecido(calculaMaterial(pedido.bojoPreto))+'</dd>'+
+            '<dt>Espuma</dt>'+
+            '<dd>'+materialGasto.espuma.toFixed(1)+'</dd>'+
+            '</dl>'+
+            '</div>';
+
+        materialGastoList.append(boxMaterialGasto).hide().fadeIn(1000);
+
+        materialGasto.espuma = 0;
+
         $('form').trigger('reset');
     }
 
@@ -100,23 +130,46 @@ $(document).ready(function () {
      * e a produção é sempre 10% maior para compensar falhas na produção
      */
     function calculaMaterial(qtdeBojoPedido) {
+
+        var quantidade = 0;
+
+        if(qtdeBojoPedido == "" || qtdeBojoPedido == 0 || qtdeBojoPedido == null)
+            return 0;
+
         // Acrescente 10% à quantidade solicidada
         var pedidoMais10p = (qtdeBojoPedido * 1.1).toFixed();
+
         // Divide por 8
         var dividido = (pedidoMais10p / placa).toFixed();
 
         //Se a quantidade solicitada não for um múltiplo de 8, então serão produzido mais bojos para evitar
         // que a prensa seja sub-utilizada.
-        var quantidade = (qtdeBojoPedido % placa != 0 ? dividido * placa : (dividido * placa) + placa);
+         quantidade = (qtdeBojoPedido % placa != 0 ? dividido * placa : (dividido * placa) + placa);
 
         /* Mensagem para debug */
         var resultado = 'Quantidade produzida: '+quantidade+
-                        '. Pedido + 10%: '+pedidoMais10p+
-                        '. '+pedidoMais10p+' dividido por '+placa+' = '+dividido+', sobra '+pedidoMais10p % placa;
+            '. Pedido + 10%: '+pedidoMais10p+
+            '. '+pedidoMais10p+' dividido por '+placa+' = '+dividido+', sobra '+pedidoMais10p % placa;
 
         console.log(resultado);
 
+        calcularTotalEspuma(quantidade);
+
         return quantidade;
+    }
+
+   function calcularTotalEspuma(qntBojoPedido){
+
+        qntBojoPedido = qntBojoPedido * (0.15);
+        totalBojo += qntBojoPedido;
+        materialGasto.espuma = qntBojoPedido;
+
+    }
+
+    function calculaTecido(producao){
+
+        producao =  producao * (0.05);
+        return producao.toFixed(1);
     }
 
 });
